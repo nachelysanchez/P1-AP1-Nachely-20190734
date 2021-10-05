ff7b3f96608de07e5cc4176a6f8c59d3a958cf41
nachelysanchez <nashely-5@hotmail.com>
2021-10-05 17:36:58 -0400

    Creando Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_Nachely_20190734.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Nachely_20190734.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Aportes> Aportes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\AportesControl.db");
+        }
+    }
+}
